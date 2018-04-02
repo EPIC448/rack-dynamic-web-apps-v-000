6c0a79262a2e448bb2d08ba3f3f6394efec782c8
Sam. O <samrey2018@gmail.com>
2018-04-02 02:19:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,15 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
-  end
+  num_1 = kernel.rand(1..10)
+  num_2= kernel.rand(1..10)
+  num_3 = kernel.rand(1..10)
 
+  if num_1==num_2 && num_2==num_3
+    resp.write "You Win"
+  else
+    resp.write "You Lose"
+
+  end
+resp.finish
 end
